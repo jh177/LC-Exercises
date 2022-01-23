1edc9d1dc11f92cd1d892402b7e487de4286244a
jh177 <jameshong177@gmail.com>
2022-01-22 22:58:41 -0800

    Edit 49

--- array_medium_49.py
+++ array_medium_49.py
@@ -14,34 +14,6 @@ def groupAnagrams(strs):
     return checked.values()
 
 
-def isAnagram(str1, str2):
-    if len(str1) != len(str2):
-        return False
-    
-    sorted_str1 = "".join(sorted(str1))
-    sorted_str2 = "".join(sorted(str2))
-
-    return sorted_str1 == sorted_str2
-    # char_count = {}
-
-    # for char in str1:
-    #     if char not in char_count:
-    #         char_count[char] = 1
-    #     else:
-    #         char_count[char] += 1
-
-    # for char in str2:
-    #     if char not in char_count:
-    #         return False
-    #     else:
-    #         char_count[char] -= 1
-
-    # for k,v in char_count.items():
-    #     if v != 0: return False
-
-    # return True
-
-
 # strs = ["eat","tea","tan","ate","nat","bat"]
 strs = ["", "b", ""]
 print(groupAnagrams(strs))
