7baad71ea6f41d5897faf8ec5e486ddfa29807d7
jh177 <jameshong177@gmail.com>
2022-01-25 21:42:15 -0800

    Add 875 py

--- bs_medium_875.py
+++ bs_medium_875.py
@@ -1,21 +1,20 @@
+from math import *
+
+
 def minEatingSpeed(piles, h):
-        if len(piles) == h: return max(piles)
+    l = 0
+    r = max(piles)
 
-        max_pile = max(piles)
-        min_pile = min(piles)
-        
-        extra_hours = h - len(piles)
-        
-        min_k = max_pile // extra_hours        
-        k = min_k
-        
-        while k < max_pile:
-            if bs(piles, k):
-                return True
-            
+    while l + 1 < r:
+        mid = (l + r) // 2
+        if (sum(ceil(i/mid) for i in piles)) > h:
+            l = mid
+        else:
+            r = mid
 
-
-def bs(piles, k):
-    if max(piles <= 0): return True
-
-
+    return r  
+    
+    
+piles = [3, 6, 7, 11]
+h = 8
+print(minEatingSpeed(piles, h))
